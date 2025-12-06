53f01449ff213804ce9b930cf2eaa01f053b3ae4
idocollab <ryuzaki.till@gmail.com>
2025-12-06 22:53:24 +0545

    Updating README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # helper
 Helper module for python projects to replace big lines of codes.
+
+      -        < Usage Of get_input()  >-     
+
+roll_number=get_input("Enter your roll number : ",data_type=int,num_check=True)
+number=get_input("Enter any number : ",data_type=float)
+word=get_input("Enter Your Name :",data_type=str)
+print(f"Hey {word},Your Roll Number : {roll_number} and any number : {number}")
+
+
+         -     < Usage Of log_setup()  >-   
+
+            > import helper
+            > helper.log_setup()
+
+
+    
+
--- helper.py
+++ helper.py
@@ -31,19 +31,4 @@ def get_input(command,data_type=str,error="Wrong Data...",num_check=False):
      except Exception as e:
         print(e) 
 
-"""       -     < Usage Of get_input()  >-     
-
-roll_number=get_input("Enter your roll number : ",data_type=int,num_check=True)
-number=get_input("Enter any number : ",data_type=float)
-word=get_input("Enter Your Name :",data_type=str)
-print(f"Hey {word},Your Roll Number : {roll_number} and any number : {number}")
-
-
-         -     < Usage Of log_setup()  >-   
-
-            > import helper
-            > helper.log_setup()
-
-
-"""     
 
